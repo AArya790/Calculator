be70260b1e6f494de5804a9fd7fb0dd631e9dda6
Anushka Arya <anushka140790@outlook.com>
2021-03-14 12:17:42 +0000

    initial commit

---
+++ paras.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Cal_Data_Core.Model;
+
+namespace Cal_Data
+{
+    class paras
+    {
+        public void Data()
+        {
+            List<UserModel> allUsers = new List<UserModel>();
+
+            string txtData = "";
+            var rows = txtData.Split("");
+            foreach (var row in rows)
+            {
+                var columns = row.Split("");
+                var user = new UserModel();
+                user.Email = columns[0];
+                user.Password = columns[1];
+                allUsers.Add(user);
+            }
+
+            var currentUser = allUsers.FirstOrDefault(x => x.Email == "xyz");
+
+            if (currentUser.IsVerified)
+            {
+                //s
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
